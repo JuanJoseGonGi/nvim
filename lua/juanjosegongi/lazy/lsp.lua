5ed3c9bc533bb6dff33f1c94c47ad95d34fc260d
Juan José González Giraldo <juanjose.eng@gmail.com>
2024-09-09 11:58:47 -0500

    remove tsserver from required mason servers

--- lua/juanjosegongi/lazy/lsp.lua
+++ lua/juanjosegongi/lazy/lsp.lua
@@ -35,7 +35,6 @@ return {
                 "biome",
                 "gopls",
                 "lua_ls",
-                "tsserver",
                 "volar",
             },
             handlers = {
